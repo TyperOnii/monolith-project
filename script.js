6d6337715e204400f0df92ba1d5a5587e0abd971
Ruslan Nuriev <cmetall@rambler.ru>
2024-05-03 19:18:44 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const headerLogo = document.querySelector(".header__logo");
 const mobileBar = document.querySelector(".mobile-bar");
 
+// Реализация бургер меню
 headerLogo.addEventListener("click", (event) => {
   mobileBar.classList.toggle("translate-x");
 });
