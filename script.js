2ee827a502532893f072cff13f64164cba89cd96
Ruslan Nuriev <cmetall@rambler.ru>
2024-05-13 22:11:11 +0300

    form and faq

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const mobileBar = document.querySelector(".mobile-bar");
 headerLogo.addEventListener("click", (event) => {
   mobileBar.classList.toggle("translate-x");
 });
+
+// Реализация табов для секции "Часто задаваемые вопросы"
+const faqList = document.querySelector(".faq__list");
+faqList.addEventListener("click", (event) => {
+  let target = event.target.closest("[data-action]");
+
+  if (!target) return;
+  if (target.dataset.action == "open-faq") {
+    target.closest("li").nextElementSibling.classList.remove("visually-hidden");
+  }
+  if (target.dataset.action == "close-faq") {
+    target.closest("li").classList.add("visually-hidden");
+  }
+});
