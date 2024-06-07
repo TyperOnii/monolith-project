b18af8480bbc3c5a2504a8b98e928df9aabc674b
Ruslan Nuriev <cmetall@rambler.ru>
2024-06-07 18:09:02 +0300

    quiz fix

--- quiz.js
+++ quiz.js
@@ -1,19 +1,20 @@
-const quizesContainer = document.querySelector(".quiz-section");
+const quizesContainer = document.querySelector(".quiz");
 
 quizesContainer.addEventListener("click", (event) => {
   const target = event.target;
   if (!target.closest(".form-quistions__actions")) return;
-  let currentQuiz = target.closest(".quiz");
+  let currentQuiz = target.closest(".form-quistions");
   if (
     target.closest(".button-continue") &&
-    currentQuiz.nextElementSibling.classList.contains("quiz")
+    currentQuiz.nextElementSibling.classList.contains("form-quistions")
   ) {
+    console.log(currentQuiz.nextElementSibling);
     currentQuiz.nextElementSibling.hidden =
       !currentQuiz.nextElementSibling.hidden;
     currentQuiz.hidden = !currentQuiz.hidden;
   } else if (
     target.closest(".button-back") &&
-    currentQuiz.previousElementSibling.classList.contains("quiz")
+    currentQuiz.previousElementSibling.classList.contains("form-quistions")
   ) {
     currentQuiz.previousElementSibling.hidden =
       !currentQuiz.previousElementSibling.hidden;
@@ -21,3 +22,17 @@ quizesContainer.addEventListener("click", (event) => {
   }
   return;
 });
+
+// quizesContainer.addEventListener("click", (event) => {
+//   const target = event.target.closest(".form-quistions__actions");
+//   if (!target.closest(".form-quistions__actions")) return;
+//   let currentQuiz = target.closest(".form-quistions");
+//   // console.log(currentQuiz);
+//   if (
+//     target.closest(".button-continue") &&
+//     currentQuiz.nextElementSibling.classList.contains("form-quistions")
+//   ) {
+//     console.log(currentQuiz.nextElementSibling);
+//     console.log(target);
+//   }
+// });
