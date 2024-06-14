3d8416333a5482b2679eed72756dbb3a5f1cfcf3
Ruslan Nuriev <cmetall@rambler.ru>
2024-06-14 17:13:56 +0300

    add succesful page and fix form

--- pay.js
+++ pay.js
@@ -1,7 +1,7 @@
 const cardNumber = document.getElementById("card-number");
 const cardDate = document.getElementById("card-date");
 const formPay = document.getElementById("form-pay");
-const successfulMessage = document.querySelector(".successful");
+
 cardDate.addEventListener("input", (event) => {
   const valueLength = cardDate.value;
   if (valueLength.length == 2) {
@@ -11,10 +11,4 @@ cardDate.addEventListener("input", (event) => {
   if (valueLength.length == 3 && event.data == null) {
     cardDate.value = "";
   }
-});
-
-formPay.addEventListener("submit", (event) => {
-  event.preventDefault();
-  event.currentTarget.hidden = !event.currentTarget.hidden;
-  successfulMessage.hidden = !successfulMessage.hidden;
 });
